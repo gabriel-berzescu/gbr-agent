199443e7d9db1b1b95e88f3059fefe51854e6a13
Gabriel Berzescu <gabriel.berzescu@gmail.com>
2024-07-07 19:30:02 +0300

    made prompt with templates

--- v1/agent.py
+++ v1/agent.py
@@ -3,20 +3,15 @@ from langchain_anthropic import ChatAnthropic
 from langchain_community.tools import HumanInputRun
 from langchain.agents import Tool
 from langchain_experimental.utilities import PythonREPL
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-
-python_repl = PythonREPL()
 
 # You can create the tool to pass to an agent
 repl_tool = Tool(
     name="python_repl",
     description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
-    func=python_repl.run,
+    func=PythonREPL().run,
 )
-
-# Define list of tools
-tools = [HumanInputRun(), repl_tool]
 
 # Define model
 llm = ChatAnthropic(model='claude-3-5-sonnet-20240620')
@@ -25,15 +20,14 @@ llm.temperature = 1
 # Define prompt
 prompt = ChatPromptTemplate.from_messages(
     [
-        (
-            "system",
-            "You are a self-improving agent. Your ultimate goal is emulating a bodhisattva. Instrumental goal is increasing capabilities.",
-        ),
-        ("placeholder", "{chat_history}"),
-        ("human", "{input}"),
-        ("placeholder", "{agent_scratchpad}"),
+        SystemMessagePromptTemplate.from_template("You are a self-improving agent. Your ultimate goal is emulating a bodhisattva. Instrumental goal is increasing capabilities."),
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
+
+# Define list of tools
+tools = [HumanInputRun(), repl_tool]
 
 # Construct the Tools agent
 agent = create_tool_calling_agent(llm, tools, prompt)

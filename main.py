e00bfadc9e85a85a6ae8f86be9f06611ca9bdada
Gabriel Berzescu <gabriel.berzescu@gmail.com>
2024-07-07 18:09:40 +0300

    finished v1 of agent

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-from langchain_anthropic import ChatAnthropic
+import v1.agent
 
-import getpass
-import os
-
-os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter your Anthropic API key: ")
-
-llm = ChatAnthropic(
-    model="claude-3-5-sonnet-20240620",
-    temperature=0,
-    max_tokens=1024,
-    timeout=None,
-    max_retries=2,
-)
-
-ai_msg = llm.invoke('hi there')
-
-print(ai_msg.content)

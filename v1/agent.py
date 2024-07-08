865573b111b570450a41658cd57f8c8b4e8a6487
Gabriel Berzescu <gabriel.berzescu@gmail.com>
2024-07-08 22:58:37 +0300

    added yt tool and agent as tool

--- v1/agent.py
+++ v1/agent.py
@@ -1,21 +1,36 @@
 # Import necessary packages
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools import HumanInputRun
-from langchain.agents import Tool
+from langchain.agents import Tool, tool
 from langchain_experimental.utilities import PythonREPL
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 
-# You can create the tool to pass to an agent
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+# Python Interpreter Tool
 repl_tool = Tool(
     name="python_repl",
     description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
     func=PythonREPL().run,
 )
 
+# YouTube Transcript Tool
+@tool
+def youtube_transcript(video_id: str) -> str:
+    """Given the ID of a YouTube video, this tool will return it's transcript."""
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatted_transcript = TextFormatter().format_transcript(transcript)
+    return formatted_transcript
+
 # Define model
-llm = ChatAnthropic(model='claude-3-5-sonnet-20240620')
-llm.temperature = 1
+llm = ChatAnthropic(
+    model="claude-3-5-sonnet-20240620",
+    temperature=1,
+    timeout=None,
+    max_retries=2,
+)
 
 # Define prompt
 prompt = ChatPromptTemplate.from_messages(
@@ -26,13 +41,22 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# Define list of tools
-tools = [HumanInputRun(), repl_tool]
+# Agent As Tool
+@tool
+def agent_as_tool(task: str) -> str:
+    """Secondary agent for sub-tasks, to prevent overloading your context window."""
+    # create a list of tools that shall be used by the agent
+    tools = [HumanInputRun(), repl_tool, youtube_transcript, agent_as_tool]
 
-# Construct the Tools agent
-agent = create_tool_calling_agent(llm, tools, prompt)
+    # initialize agent
+    agent = create_tool_calling_agent(llm, tools, prompt)
 
-# Create an agent executor by passing in the agent and tools
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=1000)
-agent_executor.invoke({"input": "After outputting text, always invoke either the human tool or the python tool. Never output just text, because this would finish the execution. This is very important."})
+    # the AgentExecutor (which will repeatedly call the agent and execute tools)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=1000)
+
+    # run the agent
+    return agent_executor.invoke({"input": task})
+
+# invoke the agent as tool
+agent_as_tool.invoke("After outputting text, always invoke either the human tool or the python tool. Never output just text, because this would finish the execution. This is very important.")
 
